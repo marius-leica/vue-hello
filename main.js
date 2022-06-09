c33c558257a704159dfe08601419778acf0c353f
marius-leica <mariusleica@gmail.com>
2022-06-09 15:17:19 +0200

    Stampato in pagina un testo da data.

---
+++ main.js
@@ -0,0 +1,15 @@
+const appVue = new Vue({
+  // selettore dell'elemento html dove avviare vue
+  el: "#app",
+
+  // Conterrà tutti i dati necessari all'applicazione
+  data: {
+    // varie chiavi necessarie all'app
+    message: "Ciao! Sono stato stampato da Vue.js",
+  },
+
+  // Conterrà una serie di funzioni
+  methods: {
+    // funzioni che useremo nell'app
+  },
+});
